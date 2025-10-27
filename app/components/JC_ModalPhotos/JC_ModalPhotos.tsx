f541aa47f02cf82cc856746d303ff0a02772d396
sosroInSpace <mathewbowyer@hotmail.com>
2025-10-27 20:29:44 +0800

    fixed class to className

--- app/components/JC_ModalPhotos/JC_ModalPhotos.tsx
+++ app/components/JC_ModalPhotos/JC_ModalPhotos.tsx
@@ -849,7 +849,7 @@ export default function JC_ModalPhotos(
                                                         width="150px"
                                                         height="150px"
                                                         viewBox="0 0 1024 1024"
-                                                        class="icon"
+                                                        className="icon"
                                                         version="1.1"
                                                         xmlns="http://www.w3.org/2000/svg"
                                                     >
