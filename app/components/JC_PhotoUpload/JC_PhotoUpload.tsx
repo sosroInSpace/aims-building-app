cd3e82ca92ebd2337d75c56f3409c7948a6dbded
sosroInSpace <mathewbowyer@hotmail.com>
2025-10-27 20:23:07 +0800

    fixed svg bug

--- app/components/JC_PhotoUpload/JC_PhotoUpload.tsx
+++ app/components/JC_PhotoUpload/JC_PhotoUpload.tsx
@@ -258,7 +258,7 @@ export default function JC_PhotoUpload(_: Readonly<JC_PhotoUploadProps>) {
                                                         width="150px"
                                                         height="150px"
                                                         viewBox="0 0 1024 1024"
-                                                        class="icon"
+                                                        className="icon"
                                                         version="1.1"
                                                         xmlns="http://www.w3.org/2000/svg"
                                                     >
